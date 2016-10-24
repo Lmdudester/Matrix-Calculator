c1503654b0bc758609a96157ecff0822e3b6dd17
Liam Davies <lmdudester@gmail.com>
2016-10-24 02:51:08 -0400

    add: MatrixArithmetic.java with add method for adding

---
+++ matrix/MatrixArithmetic.java
@@ -0,0 +1,27 @@
+package matrix;
+
+import java.lang.IllegalStateException;
+
+public class MatrixArithmetic {
+	
+	/*
+	 * _____add_____
+	 * - Adds two Matrices and returns the resulting matrix
+	 * - Throws an error if sizes don't match
+	 */
+	public static Matrix add(Matrix m1, Matrix m2){
+		if(m1.rows != m2.rows || m1.columns != m2.columns){
+			throw new IllegalStateException("Cannot Add Matricies of Different Sizes.");
+		}
+		
+		Matrix result = new Matrix(m1.rows, m1.columns);
+		
+		for(int r = 0; r < m1.rows; r++){
+			for(int c = 0; c < m1.columns; c++){
+				result.changeIndex(r, c, (m1.accessIndex(r, c) + m2.accessIndex(r, c)));
+			}
+		}
+		
+		return result;
+	}
+}
