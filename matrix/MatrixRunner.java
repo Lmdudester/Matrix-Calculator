2cb60c95919d186fde8af7ee22753a480ecdc8de
Parsons-David <parsonsd97@gmail.com>
2016-11-02 00:16:43 -0400

    Created Runner Class, that runs calculator method, taking matrices from files passed from command line arguements

---
+++ matrix/MatrixRunner.java
@@ -0,0 +1,20 @@
+
+public class MatrixRunner {
+
+	public static void main(String[] args) {
+		for(String s : args){
+			System.out.println(s);
+		}
+		Matrix m1 = MatrixMaker.fMakeMatrix(args[0]);
+		Matrix m2 = MatrixMaker.fMakeMatrix(args[1]);
+
+		System.out.println("Matrix 1:\n" + m1.toString());
+		System.out.println("Matrix 2:\n" + m1.toString());
+
+		Matrix sum = MatrixArithmetic.add(m1, m2);
+
+		System.out.println("Matrix Sum:\n" + sum.toString());
+
+	}
+
+}
