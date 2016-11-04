e990d0d7c6ed5c4648e7492da73590d9dfc3406e
Liam Davies <lmdudester@gmail.com>
2016-11-03 23:45:30 -0400

    Add: .bat files

---
+++ MatrixCalc.bat
@@ -0,0 +1,11 @@
+
+javac matrix/Matrix.java
+javac gui/MatrixCalculatorWindow.java
+javac gui/MatrixCalculator.java
+java gui/MatrixCalculator
+
+del /f gui/MatrixCalculatorWindow.class
+del /f gui/MatrixCalculator.class
+del /f matrix/Matrix.class
+
+pause
